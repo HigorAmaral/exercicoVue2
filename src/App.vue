a3e425a12182f06fe71e95abc7c8cac069aa42e5
higoramaral <amarahigor@gmail.com>
2024-03-20 11:57:51 -0300

    Inicio do Projetinhoooo

--- src/App.vue
+++ src/App.vue
@@ -1,132 +1,91 @@
 <script setup>
-  import { computed, ref } from 'vue';
-
-  const cidade = ref('');
-  const cidades = ref([
-    'São Paulo',
-    'Rio de Janeiro',
-    'Belo Horizonte',
-    'Salvador',
-    'Fortaleza',
-    'Curitiba',
-    'Manaus',
-    'Recife',
-    'Porto Alegre',
-    'Brasília',
-  ]);
-
-  const cidadesOrdenadas = computed(() => [...cidades.value].sort());
-
-  function adicionarCidade() {
-    cidades.value.push(cidade.value);
-    cidade.value = '';
-  }
-
-  function removerCidade(index) {
-    cidades.value.splice(index, 1);
-  }
+import { ref } from 'vue'
 </script>
 
 <template>
-  <div class="exercicio">
-    <h1>Exercício</h1>
-    <div class="form">
-      <h2>Formulário de cidades</h2>
-      <input type="text" v-model="cidade" />
-      <button @click="adicionarCidade">Adicionar</button>
-    </div>
-    <div class="resultados">
-      <div class="cidades">
-        <h2>Lista de cidades</h2>
-        <ul>
-          <li v-for="(cidade, index) in cidades" :key="index">
-             {{ cidade }} 
-            <button @click="removerCidade(index)">Remover</button>
-          </li>
-        </ul>
-      </div>
-      <div class="cidades">
-        <h2>Cidades ordenadas</h2>
-        <ul>
-          <li v-for="cidade in cidadesOrdenadas" :key="cidade">
-             {{ cidade }} 
-          </li>
-        </ul>
-      </div>
-    </div>
+  <div class="form">
+    <h1>Formulário</h1>
+    <input type="text" v-model="nome" placeholder="Nome" required />
+
+    <input type="email" v-model="email" placeholder="Email" required />
+
+    <input type="password" v-model="senha" placeholder="Senha" required />
+
+    <input type="password" v-model="confirmaSenha" placeholder="Confirme a senha" required />
+
+    <label for="nascimento">Data de Nascimento</label>
+    <input type="date" v-model="nascimento" placeholder="Data de nascimento" required />
+
+    <input type="text" v-model="endereco" placeholder="Endereço" required />
+
+    <input type="text" v-model="cidade" placeholder="Cidade" required />
+
+    <input type="text" v-model="estado" placeholder="Estado" />
+
+    <select id="estado" name="estado">
+      <option value="AC">Acre</option>
+      <option value="AL">Alagoas</option>
+      <option value="AP">Amapá</option>
+      <option value="AM">Amazonas</option>
+      <option value="BA">Bahia</option>
+      <option value="CE">Ceará</option>
+      <option value="DF">Distrito Federal</option>
+      <option value="ES">Espírito Santo</option>
+      <option value="GO">Goiás</option>
+      <option value="MA">Maranhão</option>
+      <option value="MT">Mato Grosso</option>
+      <option value="MS">Mato Grosso do Sul</option>
+      <option value="MG">Minas Gerais</option>
+      <option value="PA">Pará</option>
+      <option value="PB">Paraíba</option>
+      <option value="PR">Paraná</option>
+      <option value="PE">Pernambuco</option>
+      <option value="PI">Piauí</option>
+      <option value="RJ">Rio de Janeiro</option>
+      <option value="RN">Rio Grande do Norte</option>
+      <option value="RS">Rio Grande do Sul</option>
+      <option value="RO">Rondônia</option>
+      <option value="RR">Roraima</option>
+      <option value="SC">Santa Catarina</option>
+      <option value="SP">São Paulo</option>
+      <option value="SE">Sergipe</option>
+      <option value="TO">Tocantins</option>
+    </select>
+
+    <input type="text" v-model="hobbies" placeholder="Hobbies" />
+
+    <input type="text" v-model="linguagens" placeholder="Suas Linguagens De Prog..." />
+
+    <label for="Biografia">Faça sua Biografia</label>
+    <textarea name="biografia" id="biografia" cols="30" rows="20" maxlength="5000"></textarea>
   </div>
 </template>
 
-<style scoped>
-  button {
-    padding: 10px;
-    border: none;
-    border-radius: 5px;
-    background-color: #007bff;
-    color: #fff;
-    cursor: pointer;
-  }
+<style>
+.form {
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(235, 230, 171);
+  height: 40%;
+  width: 40%;
+  align-items: center;
+  margin: 0 auto;
+}
 
-  button:hover {
-    background-color: #0056b3;
-  }
+input {
+  margin-top: 5px;
+  margin-bottom: 5px;
+  height: 80%;
+  width: 80%;
+}
 
-  .exercicio {
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
+select {
+  height: 80%;
+  width: 80%;
+}
 
-  .form {
-    margin: 20px auto;
-    background-color: #f5f5f5;
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    width: 70%;
-  }
-
-  .form input {
-    padding: 10px;
-    margin-right: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    width: 50%;
-  }
-
-  .resultados {
-    display: flex;
-    justify-content: space-between;
-    width: 70%;
-  }
-
-  .cidades {
-    margin-bottom: 20px;
-    background-color: #f5f5f5;
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    width: 40%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .cidades ul {
-    list-style: none;
-    padding: 0;
-    width: 80%;
-  }
-
-  .cidades li {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 10px;
-  }
-
-  .cidades button {
-    margin-left: 10px;
-  }
+textarea {
+  height: 80%;
+  width: 80%;
+}
 </style>
